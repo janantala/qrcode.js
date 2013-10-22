e025a8493f68aa72f0b38779d12a713f00997cc7
Christian Haas <christian.haas@sevensuns.at>
2013-10-22 15:40:55 +0200

    Created Grunt.js based build * provided package.json file * separated qrcode.js into separate files under src/ using common.js format * provided .jshintrc and .jsbeautifyrc files, made sources adhere to rules * Gruntfile.js using jshint, jsbeautifier, browserify and uglify to compile into lib/ directory

---
+++ src/ALPHA_NUM.js
@@ -0,0 +1,32 @@
+var QRMode = require("./QRMode.js");
+
+//---------------------------------------------------------------------
+// ALPHA_NUM
+//---------------------------------------------------------------------
+
+function ALPHA_NUM(data) {
+  this.mode = QRMode.MODE_ALPHA_NUM;
+  this.data = data;
+}
+
+ALPHA_NUM.prototype.getCode = function(code) {
+  var codes = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:";
+  return codes.indexOf(code);
+};
+
+ALPHA_NUM.prototype.getLength = function(buffer) {
+  return this.data.length;
+};
+
+ALPHA_NUM.prototype.write = function(buffer) {
+  for (var i = 0; i < this.data.length; i++) {
+    if (this.data[i + 1]) {
+      buffer.put((45 * this.getCode(this.data[i])) + this.getCode(this.data[i + 1]), 11);
+    } else {
+      buffer.put(this.getCode(this.data[i]), 6);
+    }
+    i++;
+  }
+};
+
+module.exports = ALPHA_NUM;
---
+++ src/NUMBER.js
@@ -0,0 +1,44 @@
+var QRMode = require("./QRMode.js");
+
+//---------------------------------------------------------------------
+// NUMBER
+//---------------------------------------------------------------------
+
+function NUMBER(data) {
+  this.mode = QRMode.MODE_NUMBER;
+  this.data = data;
+}
+
+NUMBER.prototype.getBitLen = function(length) {
+  var NUMBER_LENGTH = {
+    3: 10,
+    2: 7,
+    1: 4
+  };
+  return NUMBER_LENGTH[length];
+};
+
+NUMBER.prototype.getLength = function(buffer) {
+  return this.data.length;
+};
+
+NUMBER.prototype.write = function(buffer) {
+  for (var i = 0; i < this.data.length; i++) {
+
+    var chars = this.data[i];
+    if (this.data[i + 1]) {
+      chars += this.data[i + 1];
+    }
+    if (this.data[i + 2]) {
+      chars += this.data[i + 2];
+    }
+
+    var bitLength = this.getBitLen(chars.length);
+    buffer.put(parseInt(chars, 10), bitLength);
+
+    i++;
+    i++;
+  }
+};
+
+module.exports = NUMBER;
